dcbadcec2c6ec4b58a1e570c2e69fdd95692a5c5
AntonMitchenko <72133032+AntonMitchenko@users.noreply.github.com>
2020-10-07 15:24:18 +0300

    Add files via upload

---
+++ 2 task_MitchenkoKM-03.py
@@ -0,0 +1,17 @@
+user_name = input('Enter your name: ')
+user_name2 = input('Enter your surname: ')
+telehpone_number  = input('Enter your number')
+Home =str(input(('Street')))
+house_number = input('house number')
+flat_number= input('flat number')
+City = input('City')
+Index = input('Index')
+Country = input('Country')
+
+
+
+print('Name and surename:', user_name, user_name2)
+print('Your telephone number:',telehpone_number)
+print('Home:',Home,house_number,flat_number,City)
+print('Index:',Index)
+print('Country:',Country)
